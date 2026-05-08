cd7b711b1d5ae393fa22df70ab0c51ebc46ba10a
StaymateHood <staymatehoodgithub@gmail.com>
2026-05-08 16:04:47 +0530

    Add files via upload

---
+++ env.php
@@ -0,0 +1,21 @@
+<?php
+$secret_key = "e3ff5f077839c1331b1d893a728246685cb7dba9e3a77bffe7d52eaccf660988";
+$development_mode = true;
+
+// Cashfree Configuration - Replace with your actual credentials
+$cashfree_app_id = "TEST108407170f9dcb8a8ecc11316bd071704801";
+$cashfree_secret_key = "cfsk_ma_test_2ef081550ecb0b7c7113eb07889d57c8_32f64ddd";
+
+//Cashfree Payout configuaration
+$Client_ID = "CF10730547D2G2030026KC73BDER2G";
+$Client_Secret = "cfsk_ma_test_e78632ff759dc51a807e44be2ea52e7c_7536afb8";
+
+$gateway_type = "razorpay"; // or "cashfree"
+// $razorpay_key = "rzp_test_RnUE0LNaWhqEA5";
+// $razorpay_secret = "0XyTKqXWGa42H7AIF6pP6Y0Y";
+$razorpay_key = "rzp_live_RoydMJ62p045iA";
+$razorpay_secret = "yfP9t07u2DmHLFf6eb4KPWLd";
+$razorpay_webhook_secret = "a3f4b2c9d6e8f1a07b4c9d2e5f6a198bb0c7d9e5a1f2b3c4d5e6f7a8b9c0d1e";
+
+
+?>
